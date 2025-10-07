1e499a09cec921df6f35674423456371b4a9f55c
Mira Abu Rahmeh <mira.a@b-tek.dev>
2025-10-07 10:26:35 +0300

    firt commit

---
+++ src/main/java/com/example/testSemgrep2/TestSemgrep2Application.java
@@ -0,0 +1,13 @@
+package com.example.testSemgrep2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSemgrep2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSemgrep2Application.class, args);
+	}
+
+}
